28d055944bfc6f6aaaf949337c8649f57e446de7
choudharykartik <38472520+choudharykartik@users.noreply.github.com>
2021-05-20 04:03:28 +0530

    add model

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=120,null=True,blank=True)
+    desc = models.CharField(max_length=250,null=True,blank=True)
+    
