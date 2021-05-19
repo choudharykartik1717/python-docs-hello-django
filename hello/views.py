1a79a07983aa013fc8a728659df660946fa0e428
choudharykartik <38472520+choudharykartik@users.noreply.github.com>
2021-05-20 04:26:45 +0530

    update for test latest code

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, This is STY Webapp hosted on Azure")
+    return HttpResponse("Hello, This is STY Webapp hosted on Azure updated")
